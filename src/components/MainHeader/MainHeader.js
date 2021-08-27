8be37d4863fac061a1051e53bb30fcdfeb1d7178
Ivan Escalante <ivan-enrique.escalante-cornejo@ibm.com>
2021-08-26 22:59:40 -0400

    modified Header title

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from "./MainHeader.module.css";
 const MainHeader = () => {
   return (
     <header className={classes["main-header"]}>
-      <h1>A Typical Page</h1>
+      <h1>Login Page</h1>
       <Navigation />
     </header>
   );
