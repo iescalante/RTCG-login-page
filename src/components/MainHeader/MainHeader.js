f0d6df9a843d6ff9a1ce5a477d2b9648a841c6e1
Ivan Escalante <ivan-enrique.escalante-cornejo@ibm.com>
2021-08-26 23:07:20 -0400

    dynamic header title

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Navigation from "./Navigation";
 import classes from "./MainHeader.module.css";
+import AuthContext from "../../store/auth-context";
 
 const MainHeader = () => {
+  const ctx = useContext(AuthContext);
+  const header = ctx.isLoggedIn ? "Main Page" : "Login Page";
   return (
     <header className={classes["main-header"]}>
-      <h1>Login Page</h1>
+      <h1>{header}</h1>
       <Navigation />
     </header>
   );
